104bd308f053aed7c1443edcedf3c23cd7c15d0e
Sevaarcen <coleipod@gmail.com>
2019-09-08 12:17:35 -0400

    Update uploadprofilepic.php
    Fixed incorrect syntax of matching file extension using preg_match(). Refactored code.

--- uploadprofilepic.php
+++ uploadprofilepic.php
@@ -1,10 +1,10 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] != 'POST') {
 		$end_url = "index.php";
-    ob_start();
-    header('Location: '.$end_url);
-    ob_end_flush();
-    die();
+    		ob_start();
+    		header('Location: '.$end_url);
+    		ob_end_flush();
+    		die();
 	}
 
 	session_start();
@@ -12,16 +12,16 @@
 	$username = $_SESSION['username'];
 	$encoded_username = urlencode($username);
 
-	$target_file = $target_dir.$encoded_username."_".$_FILES["picture"]['name'];
+	$target_file = $target_dir.$encoded_username."_".$_FILES["picture"]["name"];
 
 	$filetype = $_FILES['picture']['type'];
-	if (!preg_match('#image/.*#', $filetype) || !preg_match("#\.(jpe?g)|(png)|(gif)#")) {
+	if (!preg_match('#image/.*#', $filetype) || !preg_match("#\.(jpe?g)|(png)|(gif)#", $_FILES["picture"]["name"])) {
 		$_SESSION['upload_message'] = "Error: bad filetype. It must be a JPEG/JPG, a PNG, or a GIF!";
 		$end_url = "profile.php";
-  	ob_start();
-  	header('Location: '.$end_url);
-  	ob_end_flush();
-  	die();
+  		ob_start();
+  		header('Location: '.$end_url);
+  		ob_end_flush();
+  		die();
 	}
 
 	move_uploaded_file($_FILES['picture']['tmp_name'], $target_file);
@@ -31,10 +31,10 @@
 	$query = "UPDATE Users SET profile_picture='$target_file' WHERE username='$username'";
 	$result = mysqli_query($connection, $query);
 
-	$_SESSION['upload_message'] = "Success! Image has been updated and is at: ".$target_file;
-  $end_url = "profile.php";
-  ob_start();
-  header('Location: '.$end_url);
-  ob_end_flush();
-  die();
+	$_SESSION['upload_message'] = "Success! Image has been uploaded.";
+	$end_url = "profile.php";
+	ob_start();
+	header('Location: '.$end_url);
+	ob_end_flush();
+	die();
 ?>
